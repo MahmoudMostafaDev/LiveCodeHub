7842311c2884dc741773ac67f1aabd6c81898f8b
Mahmoud Mostafa <meropvo17@gmail.com>
2025-02-10 08:03:30 +0200

    build the functionality of signup , login , start now buttons

--- livecodehub/src/.components/landing/Header.tsx
+++ livecodehub/src/.components/landing/Header.tsx
@@ -1,16 +1,19 @@
-import Image from 'next/image';
+
 import React from 'react';
 import Logo from '../ui/Logo';
 import Button from '../ui/Button';
 import styles from '@/styles/components/landing/Header.module.scss'
+import Link from 'next/link';
+
 
 const Header = () => {
+
     return (
         <div className={styles.header}>
             <Logo />
             <div className={styles.ctas}>
-                <Button title='Sign Up' />
-                <Button title='Login' />
+                <Link href='/auth/signup'><Button title='Sign Up' /></Link>
+                <Link href='/auth/login'>  <Button title='Login' /></Link>
             </div>
         </div>
     );
--- livecodehub/src/.components/landing/Intro.tsx
+++ livecodehub/src/.components/landing/Intro.tsx
@@ -3,6 +3,7 @@ import styles from "@/styles/components/landing/Intro.module.scss"
 import Image from 'next/image';
 import landing from "../../../public/Main/landing.png";
 import Button from '../ui/Button';
+import Link from 'next/link';
 const Intro = () => {
     return (
         <div className={styles.intro}>
@@ -10,7 +11,7 @@ const Intro = () => {
                 <h2>Learn, Code, and Grow with Live
                     Interactive Sessions!</h2>
                 <p>Join a dynamic platform designed for teachers and students to engage in real-time coding sessions. Whether you're a student eager to learn or a teacher looking to guide, LiveCodeHub offers the perfect environment to enhance your coding skills.</p>
-                <Button title='Start Now' />
+                <Link href='/auth/signup'> <Button title='Start Now' /></Link>
             </div>
             <Image src={landing} alt="landing" width={385} height={385} />
         </div>
