be215c3ce9347d3454dbd639701ffe0f0920b7a4
Mahmoud Mostafa <meropvo17@gmail.com>
2025-02-12 11:07:43 +0200

    Update Database : seperate between teacher and student as user

--- livecodehub/prisma/seed.ts
+++ livecodehub/prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, provider } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -6,25 +6,37 @@ let DATA = {
   users: [
     {
       id: 1,
-      username: "john_doe",
+      username: "john_doe1",
       password: "hashed_password_1",
       provider: "CREDENTIALS",
       email: "john@example.com",
-      last_course: 1,
+      image: "https://random.imagecdn.app/500/150",
     },
     {
-      id: 2,
-      username: "jane_smith",
+      id: 11,
+      username: "jane_smith1",
       password: "hashed_password_2",
       provider: "GOOGLE",
       email: "jane@example.com",
-      last_course: 2,
+      image: "https://random.imagecdn.app/500/150",
     },
     {
-      id: 3,
-      username: "mike_github",
+      id: 12,
+      username: "mike_github1",
       provider: "GITHUB",
       email: "mike@example.com",
+      image: "https://random.imagecdn.app/500/150",
+    },
+  ],
+  students: [
+    {
+      user_id: 10,
+    },
+    {
+      user_id: 11,
+    },
+    {
+      user_id: 12,
     },
   ],
   courses: [
@@ -49,17 +61,17 @@ let DATA = {
     {
       id: 1,
       course_id: 1,
-      user_id: 1,
+      user_id: 10,
     },
     {
       id: 2,
       course_id: 2,
-      user_id: 2,
+      user_id: 11,
     },
     {
       id: 3,
       course_id: 1,
-      user_id: 3,
+      user_id: 12,
     },
   ],
   videos: [
@@ -82,19 +94,19 @@ let DATA = {
   ],
   user_video_stops: [
     {
-      user_id: 1,
+      user_id: 10,
       video_id: 1,
       order: 1,
       course_id: 1,
     },
     {
-      user_id: 2,
+      user_id: 11,
       video_id: 2,
       order: 1,
       course_id: 2,
     },
     {
-      user_id: 3,
+      user_id: 12,
       video_id: 1,
       order: 2,
       course_id: 1,
@@ -103,13 +115,6 @@ let DATA = {
 };
 
 async function main() {
-  DATA.courses.forEach(async (course) => {
-    await prisma.course.upsert({
-      where: { id: course.id },
-      update: {},
-      create: course,
-    });
-  });
   DATA.users.forEach(async (user) => {
     await prisma.user.upsert({
       where: { id: user.id },
@@ -117,11 +122,34 @@ async function main() {
       create: { ...user, provider: user.provider as any },
     });
   });
+  DATA.students.forEach(async (student) => {
+    await prisma.students.upsert({
+      where: { user_id: student.user_id },
+      update: {},
+      create: {
+        ...student,
+      },
+    });
+  });
+  DATA.courses.forEach(async (course) => {
+    await prisma.course.upsert({
+      where: { id: course.id },
+      update: {},
+      create: course,
+    });
+  });
   DATA.course_user.forEach(async (course_user) => {
     await prisma.course_user.upsert({
       where: { id: course_user.id },
       update: {},
       create: course_user,
+    });
+  });
+  DATA.videos.forEach(async (video) => {
+    await prisma.videos.upsert({
+      where: { id: video.id },
+      update: {},
+      create: video,
     });
   });
   DATA.user_video_stops.forEach(async (user_video_stop) => {
@@ -136,13 +164,6 @@ async function main() {
       create: user_video_stop,
     });
   });
-  DATA.videos.forEach(async (video) => {
-    await prisma.videos.upsert({
-      where: { id: video.id },
-      update: {},
-      create: video,
-    });
-  });
 }
 main()
   .then(async () => await prisma.$disconnect())
@@ -151,3 +172,22 @@ main()
     await prisma.$disconnect();
     process.exit(1);
   });
+
+////////////////
+let DataS = {
+  courses: [
+    {
+      id: 1,
+      name: "FIGHT FOR FREEDOM",
+    },
+  ],
+  users: [
+    {
+      id: 1,
+      username: "maged",
+      password: "1231",
+      image: "https://random.imagecdn.app/500/150",
+    },
+  ],
+  students: [{}],
+};
