70d0805d4dbf1229a9d3f33a065d1bda57ae8cf3
Mahmoud Mostafa <meropvo17@gmail.com>
2025-02-22 14:03:43 +0200

    update sign up create user , such that it creates user - student - streak - student progress

--- livecodehub/src/utils/actions.ts
+++ livecodehub/src/utils/actions.ts
@@ -10,7 +10,8 @@ export async function signup(userData: { username: string; password: string }) {
     101: Missing username or password
     102: Password must be between 3 and 50 characters
     103 : username already used
-*/ try {
+*/
+  try {
     if (!userData.username || !userData.password)
       return {
         success: false,
@@ -23,7 +24,6 @@ export async function signup(userData: { username: string; password: string }) {
         data: null,
         error: validateUsername(userData.username.trim()),
       };
-
     if (userData.password.length < 3 || userData.password.length > 50)
       return {
         success: false,
@@ -44,20 +44,41 @@ export async function signup(userData: { username: string; password: string }) {
       };
     const validUsername = userData.username.trim();
     const hashedPassword = await hashPassword(userData.password);
-    const user = await prisma.user.create({
-      data: {
-        username: validUsername,
-        password: hashedPassword,
-      },
-      select: { id: true, username: true },
-    });
-    await prisma.students.create({
-      data: { user_id: user.id },
+    const result = await prisma.$transaction(async (tx) => {
+      const user = await prisma.user.create({
+        data: {
+          username: validUsername,
+          password: hashedPassword,
+        },
+        select: { id: true, username: true },
+      });
+      const student = await prisma.student.create({
+        data: {
+          userId: user.id,
+        },
+        select: { id: true, userId: true },
+      });
+      const streak = await prisma.streak.create({
+        data: {
+          studentId: student.id,
+        },
+      });
+      const studentProgress = await prisma.studentProgress.create({
+        data: {
+          studentId: student.id,
+          lessonWatchedToday: 0,
+          lastLessonWatchedDate: new Date(),
+        },
+      });
+      console.log(user, student, streak, studentProgress);
+      return {
+        success: true,
+        data: user,
+        error: null,
+      };
     });
     return {
-      success: true,
-      data: user,
-      error: null,
+      result,
     };
   } catch (err: any) {
     return {
@@ -74,6 +95,7 @@ export async function signup(userData: { username: string; password: string }) {
 
 // utils
 async function hashPassword(password: string) {
+  console.log("hashPassword", password);
   const scryptAsync = promisify(scrypt);
   const salt = randomBytes(16).toString("hex");
   const hashed = await scryptAsync(password, salt, 64);
