a50bf2284d3faa79f8e769456c52cf1255e9c483
Mahmoud Mostafa <meropvo17@gmail.com>
2025-02-23 06:43:39 +0200

    edit names of props , to be more accurate , and readable

--- livecodehub/src/.components/cards/MainLesson.tsx
+++ livecodehub/src/.components/cards/MainLesson.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 import styles from "@/styles/components/cards/MainLesson.module.scss";
 import CardVideo from './CardVideo';
 interface props {
-    title: string;
-    description: string
-    image: string
+    courseName: string;
+    lessonTitle: string
+    thumbnail: string
     finished: number
     lesson: number
 }
-const MainLesson: React.FC<props> = ({ title, description, image, finished, lesson }) => {
+const MainLesson: React.FC<props> = ({ courseName, lessonTitle, thumbnail, finished, lesson }) => {
     return (
         <div className={styles.card}>
-            <CardVideo title={title} image={image} />
+            <CardVideo title={courseName} image={thumbnail} />
             <div className={styles.content}>
                 <div className={styles.info}>
-                    <h2>{title}</h2>
-                    <p>Start lesson ({lesson}) :{description} </p>
+                    <h2>{courseName}</h2>
+                    <p>Start lesson ({lesson}) :{lessonTitle} </p>
                 </div>
                 <p className={styles.finished}>Finished : {finished}%</p>
             </div>
