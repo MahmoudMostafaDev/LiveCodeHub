a578d15c5bf2e0d5ce59c8cb8174e1ca6a61631b
Mahmoud Mostafa <meropvo17@gmail.com>
2025-02-23 06:48:32 +0200

    fix bug : get last course video using studentId not userId

--- livecodehub/src/app/api/courses/lastcourse/route.ts
+++ livecodehub/src/app/api/courses/lastcourse/route.ts
@@ -3,14 +3,18 @@ import { prisma } from "@/lib/prisma";
 import { getToken } from "next-auth/jwt";
 import { checkAuthorization } from "../../auth/authHelpers";
 import { equal } from "assert";
+import { getStudentId } from "@/utils/DbQuery";
 export async function GET(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const id = token?.id as string;
   const username = token?.username as string;
   try {
+    const studentId = await getStudentId(Number(id));
+    if (studentId === -1)
+      return new Response(JSON.stringify({}), { status: 401 });
     if (!checkAuthorization(username))
       return new Response(JSON.stringify({}), { status: 401 });
-    const video = await getLastVideo(id);
+    const video = await getLastVideo(studentId);
     if (!video) return new Response(JSON.stringify({}), { status: 404 });
     return new Response(JSON.stringify(video), { status: 200 });
   } catch (error) {
@@ -18,7 +22,7 @@ export async function GET(req: NextRequest) {
   }
 }
 
-async function getLastVideo(id: string) {
+async function getLastVideo(id: number) {
   const video = await prisma.video.findFirst({
     where: {
       lesson: {
